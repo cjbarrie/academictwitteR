17e6d79b65ec321b628525cc6355aa55dfb60ad6
cbarrie6 <chrisjbarrie@googlemail.com>
2021-04-19 13:33:47 +0300

    Add get_media_tweets function and add media.fields to get_tweets core function

--- R/get_tweets.R
+++ R/get_tweets.R
@@ -30,6 +30,7 @@ get_tweets <- function(q="",n=10,start_time,end_time,token,next_token=""){
     "start_time" = start_time,
     "end_time" = end_time, 		
     "tweet.fields" = "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,source,text,withheld",
+    "media.fields" = "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width", #media fields currently not appearing 
     "user.fields" = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld",
     "expansions" = "author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id",
     "place.fields" = "contained_within,country,country_code,full_name,geo,id,name,place_type"
