80947147c95878ac06fc17cabf50472a825260f8
cbarrie6 <chrisjbarrie@googlemail.com>
2021-04-26 12:23:20 +0300

    Update hex

--- man/figures/hexsticker.R
+++ man/figures/hexsticker.R
@@ -1,9 +1,10 @@
 library(hexSticker)
 
-s <- sticker("man/figures/owl.png", 
+s <- sticker("man/figures/athena.png", 
              package="academictwitteR", 
              p_size=4,p_color="black",
-             s_x=1.05, s_y=.75, s_width=.6,
+             s_x=1.05, s_y=.75,
+             s_width=.4, s_height=.5,
              h_fill="white", h_color="#1DA1F2",
              dpi = 800,
         filename="man/figures/academictwitteRhex.png")
