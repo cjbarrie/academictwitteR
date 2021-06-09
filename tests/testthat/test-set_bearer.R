ad85dd5472feb7826d8d89aa955e958bd08f5707
chainsawriot <chainsawtiney@gmail.com>
2021-06-09 16:56:07 +0200

    use httptest to mock API request; add test suite for get_all_tweets; update test for set_bearer

--- tests/testthat/test-set_bearer.R
+++ tests/testthat/test-set_bearer.R
@@ -7,3 +7,16 @@ test_that("get_bearer", {
   expect_equal(get_bearer(), "ABC")
   Sys.setenv("TWITTER_BEARER" = ORI_BEARER)  
 })
+
+with_mock_api({
+  test_that("integration with get_all_tweets", {
+    emptydir <- academictwitteR:::.gen_random_dir()
+    ORI_BEARER <- Sys.getenv("TWITTER_BEARER")
+    Sys.setenv("TWITTER_BEARER" = "")
+    expect_error(w1 <- capture_warnings(get_all_tweets(query = "#commtwitter", start_tweets = "2021-06-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z", verbose = TRUE, data_path = emptydir)))
+    Sys.setenv("TWITTER_BEARER" = ORI_BEARER)  
+    expect_error(w1 <- capture_warnings(get_all_tweets(query = "#commtwitter", start_tweets = "2021-06-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z", verbose = TRUE, data_path = emptydir)), NA)
+    unlink(emptydir)
+    Sys.setenv("TWITTER_BEARER" = ORI_BEARER)  
+  })
+})
