8475ed545c2f2b7045d045f090575c5a2d9f50eb
chainsawriot <chainsawtiney@gmail.com>
2021-06-12 13:51:53 +0200

    fix testcase again

--- tests/testthat/test-emptyreturn.R
+++ tests/testthat/test-emptyreturn.R
@@ -2,6 +2,9 @@
 ## start_capturing(simplify = FALSE)
 ## get_all_tweets("#Figliuolo", "2020-12-01T00:00:00Z", "2020-12-31T00:00:00Z", n=100000, data_path= emptydir, lang = "it", bind_tweets = FALSE)
 ## stop_capturing()
+ORI_BEARER <- Sys.getenv("TWITTER_BEARER")
+
+Sys.setenv("TWITTER_BEARER" = "ABC")
 
 with_mock_api({
   test_that("empty return, #88", {
@@ -10,3 +13,5 @@ with_mock_api({
     unlink(emptydir, recursive = TRUE)
   })
 })
+
+Sys.setenv("TWITTER_BEARER" = ORI_BEARER)
