71b44ba9745db64f71b269c347d987f79d7bb0fe
chainsawriot <chainsawtiney@gmail.com>
2021-06-12 13:42:09 +0200

    Fix testcase

--- tests/testthat/test-emptyreturn.R
+++ tests/testthat/test-emptyreturn.R
@@ -5,7 +5,7 @@
 
 with_mock_api({
   test_that("empty return, #88", {
-    emptydir <- academictwitteR::.gen_random_dir()
+    emptydir <- academictwitteR:::.gen_random_dir()
     expect_error(get_all_tweets("#Figliuolo", "2020-12-01T00:00:00Z", "2020-12-31T00:00:00Z", n=100000, data_path= emptydir, lang = "it", bind_tweets = FALSE), NA)
     unlink(emptydir, recursive = TRUE)
   })
