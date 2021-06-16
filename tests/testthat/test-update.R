9d30109a28fecca66cac834fb3de795993001299
Chung-hong Chan <chainsawtiney@gmail.com>
2021-06-16 11:12:07 +0200

    Fix #143 (#146)
    * Adding test case for #143
    
    * add .shift_sec, fix #143
    
    * Mockify the test for update
    
    * add verbose; use bind_tweets instead of bind_tweet_jsons
    
    * Remove test-update in .Rbuildignore

---
+++ tests/testthat/test-update.R
@@ -0,0 +1,37 @@
+## require(httptest)
+## start_capturing(simplify = FALSE)
+## ori_test <- "../testdata/commtwitter/"
+## test_dir <- academictwitteR:::.gen_random_dir()
+## dir.create(test_dir)
+## z <- file.copy(list.files(ori_test, full.names = TRUE, recursive = TRUE), test_dir, recursive = TRUE)
+## update_collection(test_dir, end_tweets = "2021-06-14T23:59:59Z", bind_tweets = FALSE)
+## unlink(test_dir, recursive = TRUE)
+## stop_capturing()
+
+
+
+with_mock_api({
+  test_that("No duplication #143", {
+    ori_test <- "../testdata/commtwitter/"
+    test_dir <- academictwitteR:::.gen_random_dir()
+    dir.create(test_dir)
+    z <- file.copy(list.files(ori_test, full.names = TRUE, recursive = TRUE), test_dir, recursive = TRUE)
+    capture_warnings(update_collection(test_dir, end_tweets = "2021-06-14T23:59:59Z", bind_tweets = FALSE))
+    a1 <- bind_tweets(test_dir)
+  ## a2 <- bind_tweets(ori_test)
+    testthat::expect_true(length(unique(a1$id)) == length(a1$id))
+    testthat::expect_true(length(table(a1$id)[table(a1$id) == 2]) == 0)
+    unlink(test_dir, recursive = TRUE)
+  })
+})
+
+with_mock_api({
+  test_that("verbose #143", {
+    ori_test <- "../testdata/commtwitter/"
+    test_dir <- academictwitteR:::.gen_random_dir()
+    dir.create(test_dir)
+    z <- file.copy(list.files(ori_test, full.names = TRUE, recursive = TRUE), test_dir, recursive = TRUE)
+    expect_silent(update_collection(test_dir, end_tweets = "2021-06-14T23:59:59Z", bind_tweets = FALSE, verbose = FALSE))
+    unlink(test_dir, recursive = TRUE)
+  })
+})
