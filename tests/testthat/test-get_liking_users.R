15702b7ae37b20ab1e45e0974f32f7a8d60b81b6
Chung-hong Chan <chainsawtiney@gmail.com>
2021-06-19 16:27:10 +0200

    Fix #153, add test cases (#157)

---
+++ tests/testthat/test-get_liking_users.R
@@ -0,0 +1,22 @@
+## require(httptest)
+## start_capturing(simplify = FALSE)
+## ori_test <- "../testdata/commtwitter/"
+## tweet_ids <- unique(bind_tweets(ori_test, verbose = FALSE)$id)
+## res <- get_liking_users(x = tweet_ids)
+## saveRDS(res, "../testdata/liking_users.RDS")
+## res2 <- get_liking_users("1405327120163872777")
+## stop_capturing()
+
+with_mock_api({
+  test_that("expected behavior, #153", {
+    ori_test <- "../testdata/commtwitter/"
+    tweet_ids <- unique(bind_tweets(ori_test, verbose = FALSE)$id)
+    res <- get_liking_users(x = tweet_ids)
+    prev_res <- readRDS("../testdata/liking_users.RDS")
+    expect_equal(nrow(res), nrow(prev_res))
+    res2 <- get_liking_users("1405327120163872777")
+    expect_lt(nrow(res2), 300) ## no page flipping
+  })
+})
+
+
