268ebc6113bf2c2a3b65e609efdf72c53237f117
Justin Ho <zjustin334@gmail.com>
2021-06-25 00:04:41 +0200

    test for count_all_tweets()

--- tests/testthat/test-count_all_tweets.R
+++ tests/testthat/test-count_all_tweets.R
@@ -3,13 +3,18 @@
 # ori_test <- "../testdata/commtwitter/"
 # res <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
 # saveRDS(res, "../testdata/count_all_tweets.RDS")
+# res2 <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
 # stop_capturing()
 
-# with_mock_api({
-#   test_that("expected behavior, #153", {
-#     ori_test <- "../testdata/commtwitter/"
-#     res <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
-#     prev_res <- readRDS("../testdata/count_all_tweets.RDS")
-#     expect_equal(nrow(res), nrow(prev_res))
-#   })
-# })
+with_mock_api({
+  test_that("expected behavior, #153", {
+    ori_test <- "../testdata/commtwitter/"
+    res <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
+    prev_res <- readRDS("../testdata/count_all_tweets.RDS")
+    expect_equal(nrow(res), nrow(prev_res))
+    res2 <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
+    expect_lt(nrow(res2), 99) ## no page flipping
+    expect_error(z <- capture_warnings(count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")), NA)
+    expect_identical(z, character(0))
+  })
+})
