73c89fed5965ee9a2e465cf81f5301ee963e0cf5
Justin Ho <zjustin334@gmail.com>
2021-06-24 20:57:27 +0200

    comment out tests

--- tests/testthat/test-count_all_tweets.R
+++ tests/testthat/test-count_all_tweets.R
@@ -5,11 +5,11 @@
 # saveRDS(res, "../testdata/count_all_tweets.RDS")
 # stop_capturing()
 
-with_mock_api({
-  test_that("expected behavior, #153", {
-    ori_test <- "../testdata/commtwitter/"
-    res <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
-    prev_res <- readRDS("../testdata/count_all_tweets.RDS")
-    expect_equal(nrow(res), nrow(prev_res))
-  })
-})
+# with_mock_api({
+#   test_that("expected behavior, #153", {
+#     ori_test <- "../testdata/commtwitter/"
+#     res <- count_all_tweets(query = "#commtwitter", start_tweets = "2021-03-01T00:00:00Z", end_tweets = "2021-06-05T00:00:00Z")
+#     prev_res <- readRDS("../testdata/count_all_tweets.RDS")
+#     expect_equal(nrow(res), nrow(prev_res))
+#   })
+# })
