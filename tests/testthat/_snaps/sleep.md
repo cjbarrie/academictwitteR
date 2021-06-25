e3d623677dcf59ad148956aed70b58b98d10c0c1
chainsawriot <chainsawtiney@gmail.com>
2021-06-25 15:09:05 +0200

    To please the testthat god

--- tests/testthat/_snaps/sleep.md
+++ tests/testthat/_snaps/sleep.md
@@ -1,22 +1,31 @@
-# integration with get_all_tweets
+# test .trigger_sleep
 
     Code
-      r <- readRDS("../testdata/rate_limit_res.RDS")
       academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:30"),
       verbose = TRUE, really_sleep = FALSE)
     Output
       Rate limit reached. Rate limit will reset at 2021-06-25 13:50:37 
       Sleeping for 8 seconds. 
       ================================================================================
+
+---
+
     Code
       academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:30"),
       verbose = FALSE, really_sleep = FALSE)
+
+---
+
+    Code
       academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:33"),
       verbose = TRUE, really_sleep = TRUE)
     Output
       Rate limit reached. Rate limit will reset at 2021-06-25 13:50:37 
       Sleeping for 5 seconds. 
       ================================================================================
+
+---
+
     Code
       academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:36"),
       verbose = FALSE, really_sleep = TRUE)
--- tests/testthat/test-sleep.R
+++ tests/testthat/test-sleep.R
@@ -2,12 +2,18 @@
 
 library(httr)
 
-expect_snapshot({
+test_that("test .trigger_sleep", {
   r <- readRDS("../testdata/rate_limit_res.RDS")
-  academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:30"), verbose = TRUE, really_sleep = FALSE)
-  academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:30"), verbose = FALSE, really_sleep = FALSE)
-  academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:33"), verbose = TRUE, really_sleep = TRUE)
-  academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:36"), verbose = FALSE, really_sleep = TRUE)
+  expect_snapshot({
+    academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:30"), verbose = TRUE, really_sleep = FALSE)
+  })
+  expect_snapshot({
+    academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:30"), verbose = FALSE, really_sleep = FALSE)
+  })
+  expect_snapshot({
+    academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:33"), verbose = TRUE, really_sleep = TRUE)
+  })
+  expect_snapshot({
+    academictwitteR:::.trigger_sleep(r, ref_time = as.POSIXlt("2021-06-25 13:50:36"), verbose = FALSE, really_sleep = TRUE)
+  })
 })
-
-  
