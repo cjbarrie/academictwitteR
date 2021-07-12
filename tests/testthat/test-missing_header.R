db7ef61b5898756b8b900b67a039ffbd3cc1afe8
Chung-hong Chan <chainsawtiney@gmail.com>
2021-07-12 16:06:37 +0200

    fix missing header problem, fix #192 (#193)
    This is the prophylactic fix for #192. As the error appears to be random, no actual test case can be captured. Instead, the test cases were created synthetically.

---
+++ tests/testthat/test-missing_header.R
@@ -0,0 +1,13 @@
+
+test_that("test missing header", {
+  library(httr)
+  r <- readRDS("../testdata/rate_limit_res.RDS")
+  expect_false(academictwitteR:::.check_header_rate_limit(r, verbose = FALSE))
+  crazy_r <- r
+  crazy_r$headers$`x-rate-limit-remaining` <- "1"
+  expect_true(academictwitteR:::.check_header_rate_limit(crazy_r, verbose = FALSE))
+  expect_warning(academictwitteR:::.check_header_rate_limit(crazy_r, verbose = TRUE))
+  really_crazy_r <- r
+  really_crazy_r$headers$`x-rate-limit-remaining` <- NULL
+  expect_false(academictwitteR:::.check_header_rate_limit(really_crazy_r, verbose = FALSE))
+})
