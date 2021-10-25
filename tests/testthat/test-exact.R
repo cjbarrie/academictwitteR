f5fed170a8296402afab077a9e4e6611a869266c
Chung-hong Chan <chainsawtiney@gmail.com>
2021-10-25 23:38:22 +0200

    Add test cases for #242; fix #235 (#249)

---
+++ tests/testthat/test-exact.R
@@ -0,0 +1,33 @@
+test_that("basic test", {
+  expect_equal("Black Lives Matter", build_query(query = "Black Lives Matter"))
+  expect_equal("\"Black Lives Matter\"", build_query(query = "Black Lives Matter", exact_phrase = TRUE))
+  expect_equal("\"Black Lives Matter\" (retweets_of:ACLU)", build_query(query = "Black Lives Matter", exact_phrase = TRUE, retweets_of = "ACLU"))
+  expect_equal("Black Lives Matter (retweets_of:ACLU)", build_query(query = "Black Lives Matter", exact_phrase = FALSE, retweets_of = "ACLU"))
+})
+
+## integration test
+## see https://github.com/cjbarrie/academictwitteR/issues/235#issuecomment-945080851
+
+## require(httptest)
+## start_capturing(simplify = FALSE)
+## tweets1 <-
+##   get_all_tweets(
+##     query = "Black Lives Matter",
+##     retweets_of = "ACLU",
+##     exact_phrase = TRUE, start_tweets = "2021-01-04T00:00:00Z", 
+##     end_tweets = "2021-01-04T00:45:00Z", 
+##     n = Inf)
+## stop_capturing()
+
+with_mock_api({
+  test_that("basic test", {
+    tweets1 <-
+      suppressWarnings(get_all_tweets(
+        query = "Black Lives Matter",
+        retweets_of = "ACLU",
+        exact_phrase = TRUE, start_tweets = "2021-01-04T00:00:00Z", 
+        end_tweets = "2021-01-04T00:45:00Z", 
+        n = Inf))
+    expect_true(nrow(tweets1) > 0)
+  })
+})
