0a28f2912ab4ab2ee0b8af9fa4a8abf10f08c8c4
chainsawriot <chainsawtiney@gmail.com>
2021-11-16 21:33:30 +0100

    Add `verbose` to all compliance functions

--- R/compliance.R
+++ R/compliance.R
@@ -1,9 +1,9 @@
-post_query <- function(url, params, bearer_token = get_bearer(), verbose = TRUE) {
+post_query <- function(url, params, bearer_token = get_bearer()) {
   bearer_token <- check_bearer(bearer_token)
     r <- httr::POST(url, httr::add_headers(Authorization = bearer_token), body = params, encode = "json")
     status_code <- httr::status_code(r)
     if (!status_code %in% c(200)) {
-      stop(paste("something went wrong. Status code:", httr::status_code(r)))
+      stop(paste("Something went wrong. Status code:", httr::status_code(r)))
     }
     r
 }
@@ -14,7 +14,7 @@ post_query <- function(url, params, bearer_token = get_bearer(), verbose = TRUE)
 #' 
 #' @param file a text file, each line contains a Tweet ID or user ID.
 #' @param type the type of the job, whether "tweets" or "users".
-#' @param bearer_token string, bearer token
+#' @inheritParams get_all_tweets
 #' 
 #' @return the job ID (invisibly)
 #' @export
@@ -24,19 +24,20 @@ post_query <- function(url, params, bearer_token = get_bearer(), verbose = TRUE)
 #' create_compliance_job("tweetids.txt", "tweets")
 #' }
 create_compliance_job <- function(file,
-                                  type,
-                                  bearer_token = get_bearer()){
+                                  type = "tweets",
+                                  bearer_token = get_bearer(),
+                                  verbose = TRUE){
   r <- post_query(url = "https://api.twitter.com/2/compliance/jobs",
                   bearer_token,
                   params = list("type" = type))
   rcontent <- jsonlite::fromJSON(httr::content(r, "text"))
-  cat("Job ID: ", rcontent$data$id, "\n")
+  .vcat(verbose, "Job ID: ", rcontent$data$id, "\n")
   r <- httr::PUT(rcontent$data$upload_url,
                  body = httr::upload_file(file))
   if(r$status_code == 200){
-    cat("Upload Completed")
+    .vcat(verbose, "Upload Completed", "\n")
   } else {
-    cat("Status Code: ", r$status_code)
+    stop(paste("Something went wrong. Status code:", r$status_code, "\n"))
   }
   invisible(rcontent$data$id)
 }
@@ -44,8 +45,7 @@ create_compliance_job <- function(file,
 #' List Compliance Jobs
 #' 
 #' This function lists all compliance jobs.
-#' @param type the type of the job, whether "tweets" or "users".
-#' @param bearer_token string, bearer token
+#' @inheritParams create_compliance_job
 #' 
 #' @return a data frame 
 #' @export
@@ -66,8 +66,7 @@ list_compliance_jobs <-function(type = "tweets",
 #' 
 #' This function retrieves the information for a single compliance job.
 #' 
-#' @param id string, the id of the compliance job
-#' @param bearer_token string, bearer token
+#' @inheritParams create_compliance_job
 #' 
 #' @return a data frame 
 #' @export
@@ -77,23 +76,23 @@ list_compliance_jobs <-function(type = "tweets",
 #' get_compliance_result("1460077048991555585")
 #' }
 get_compliance_result <- function(id, 
-                                  bearer_token = get_bearer()){
+                                  bearer_token = get_bearer(), verbose = TRUE){
   r <- make_query(paste0("https://api.twitter.com/2/compliance/jobs/",id),
                   params = list(), 
                   bearer_token = bearer_token)
   status <- r$data$status
   if(status == "in_progress"){
-    cat("Compliance check in progress...\n")
+    stop("Compliance check is still in progress.\n")
   } else if (status == "complete"){ 
     # Download if ready
-    cat("Downloading...\n")
+    .vcat(verbose, "Downloading...\n")
     dl <- httr::GET(r$data$download_url)
     filename <- paste0(id,".json")
     writeLines(httr::content(dl, "text", encoding = "UTF-8"), filename)
-    jsonlite::stream_in(file(filename), verbose = FALSE)
+    return(jsonlite::stream_in(file(filename), verbose = FALSE))
   } else if (status == "expired"){
-    cat("Upload expired, please retry.\n")
+    stop("Upload expired, please retry.\n")
   } else {
-    cat("Status: ", status)
+    stop(paste("Something went wrong. Status:", status, "\n"))
   }
 }
