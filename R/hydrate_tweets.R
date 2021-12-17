85f82a882dcb769baea2caab74122b92009ac81a
Tim König <32549063+TimBMK@users.noreply.github.com>
2021-12-17 14:25:10 +0100

    add `hydrate_tweets` (#260)
    * Update utils.R
    
    Added an option to retrieve erroneous tweets with their error title. Changes are made in get_tweets() and df_to_json() in order to bind the erroneous tweets to the bound dataframe and make error_-json files in the data_path
    
    * Create tweets_lookup
    
    Function to access the API v2 Tweets Lookup (https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/introduction) function. This is useful when looking to rehydrate tweets via their tweet IDs. An option to catch errors in the lookup process is added (see changes to utils for implementation)
    
    * Update tweets_lookup
    
    added total success overview at the end of the loop
    
    * Make runnable; pass all old unit tests
    
    * Add Tim König a contributor
    
    * Exclude the `errors` part for now (It will be for another PR)
    
    * Add documentation and rename `tweets_lookup` to `hydrate_tweets`
    
    * Add tests for corner cases
    
    * Add basic test cases
    
    * Clear check messages / errors
    
    * Correct documentation of `hydrate_tweets` on `context_annotations`
    
    * Update hydrate_tweets.R
    
    minor adjustment to make sure the reporting doesn't break the function when bind_tweets == F ("Retrieved" and "Total Tweets" verbose messages rely on the new_rows object which is only made if tweets are bound)
    
    * Reduce some verbosity
    
    Co-authored-by: chainsawriot <chainsawtiney@gmail.com>

---
+++ R/hydrate_tweets.R
@@ -0,0 +1,82 @@
+#' Hydrate Tweets Based On Tweet IDs
+#'
+#' This function is helpful for hydrating Tweet IDs (i.e. getting the full content of tweets from a list of Tweet IDs).
+#' @inheritParams get_all_tweets
+#' @param ids a character vector of Tweet IDs
+#' @param context_annotations If `TRUE`, context_annotations will be fetched.
+#' @return When bind_tweets is `TRUE`, the function returns a data frame. The `data_path` if `bind_tweets` is `FALSE`
+#' @examples
+#' \dontrun{
+#' hydrate_tweets(c("1266876474440761346", "1266868259925737474", "1266867327079002121",
+#' "1266866660713127936", "1266864490446012418", "1266860737244336129",
+#' "1266859737615826944", "1266859455586676736", "1266858090143588352",
+#' "1266857669157097473"))
+#' }
+#' @export
+hydrate_tweets <- function(ids,  bearer_token = get_bearer(), data_path = NULL,
+                           context_annotations = FALSE,
+                           bind_tweets = TRUE,
+                           verbose = TRUE) {
+  ## verbose = TRUE,
+  ## errors = FALSE) {
+  ## Building parameters for get_tweets()
+  if (is.null(data_path) & !bind_tweets) {
+    stop("Argument (bind_tweets = FALSE) only valid when a data_path is specified.")
+  }
+  params <- list(
+    tweet.fields = "attachments,author_id,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,source,text,withheld", 
+    user.fields = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld", 
+    expansions = "author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id", 
+    place.fields = "contained_within,country,country_code,full_name,geo,id,name,place_type"
+  )
+  if (context_annotations) {
+    params[["tweet.fields"]] <- "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,source,text,withheld"
+  }
+  ## loop through x in batches of 100 IDs
+  new_df <- data.frame()
+  if (length(ids) >= 1) {
+    n_batches <- ceiling(length(ids) / 100)
+  } else {
+    n_batches <- 0
+  }
+  endpoint_url <- "https://api.twitter.com/2/tweets"
+  for (i in seq_len(n_batches)) {
+    batch <- ids[((i-1)*100+1):min(length(ids),(i*100))]
+    params[["ids"]] <- paste0(batch, collapse = ",")
+    
+    ## Get tweets
+    .vcat(verbose, "Batch", i, "out of", ceiling(length(ids) / 100),": ids", utils::head(batch, n = 1), "to", utils::tail(batch, n = 1), "\n")
+    ## new_rows <- get_tweets(params = params, endpoint_url = endpoint_url, n = Inf, file = NULL, bearer_token = bearer_token, 
+    ##                        export_query = FALSE, data_path = data_path, bind_tweets = bind_tweets, verbose = verbose, errors = errors)
+    new_rows <- get_tweets(params = params, endpoint_url = endpoint_url, n = Inf, file = NULL, bearer_token = bearer_token, 
+                           export_query = FALSE, data_path = data_path, bind_tweets = bind_tweets, verbose = FALSE)
+    
+    if (bind_tweets) {
+    ## if (errors){
+    ##   .vcat(verbose, "Retrieved", nrow(dplyr::filter(new_rows, is.na(error))), "out of", length(batch), "\n" , 
+    ##         "Errors:", nrow(dplyr::filter(new_rows, !is.na(error))), "\n" )
+    ## } else {
+    .vcat(verbose, "Retrieved", nrow(new_rows), "out of", length(batch), "\n")
+    ## }
+
+      ##  new_rows$from_tweet_id <- batch[batch %in% new_rows$id]
+      if (nrow(new_rows) > 0) { 
+        new_df <- dplyr::bind_rows(new_df, new_rows) # add new rows
+      }
+    
+    ## if (errors) {
+    ##   .vcat(verbose, "Total Tweets:", nrow(dplyr::filter(new_df, is.na(error))), "\n")
+    ## } else {
+    .vcat(verbose, "Total Tweets:", nrow(new_df), "\n")
+    ##   }
+    ## }
+    ## if (errors) {
+    ##   .vcat(verbose, "Total of", nrow(dplyr::filter(new_df, is.na(error))), "out of", length(ids), "tweets retrieved.\n")
+    ## }
+    }
+  }
+  if (bind_tweets) {
+    return(new_df)
+  }
+  return(invisible(data_path))
+}
