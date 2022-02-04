74088ff76043c3a18388f2fdba797d166e2b5d55
chainsawriot <chainsawtiney@gmail.com>
2022-02-04 17:57:49 +0100

    Fix `test-get_retweeted_by.R` ref #270

--- tests/testthat/test-get_retweeted_by.R
+++ tests/testthat/test-get_retweeted_by.R
@@ -5,6 +5,7 @@
 
 with_mock_api({
   test_that("expected behavior, #153", {
+    skip_if(!dir.exists("api.twitter.com"))
     res <- get_retweeted_by("1423714771384340481")
     expect_equal(nrow(res), 2) ## no page flipping
   })
@@ -17,6 +18,7 @@ with_mock_api({
 
 with_mock_api({
   test_that("zero row behavior", {
+    skip_if(!dir.exists("api.twitter.com"))
     expect_error(res <- get_retweeted_by("1399797349296197635"), NA)
     expect_equal(nrow(res), 0)
   })
@@ -24,6 +26,7 @@ with_mock_api({
 
 with_mock_api({
   test_that("x is a vector", {
+    skip_if(!dir.exists("api.twitter.com"))
     expect_error(res <- get_retweeted_by(c("1399797349296197635", "1423714771384340481")), NA)
     expect_equal(nrow(res), 2)
   })
@@ -31,6 +34,7 @@ with_mock_api({
 
 with_mock_api({
   test_that("verbose behaviour", {
+    skip_if(!dir.exists("api.twitter.com"))
     expect_silent(get_retweeted_by(c("1399797349296197635", "1423714771384340481"), verbose = FALSE))
     expect_output(get_retweeted_by(c("1399797349296197635", "1423714771384340481"), verbose = TRUE))
   })
