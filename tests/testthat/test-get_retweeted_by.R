5245710e5d8c6e8ef2710e5913bb00ce1afb46b8
Tom Davidson <thomas.davidson@rutgers.edu>
2022-02-09 09:23:40 -0500

    Update `get_retweeted_by`, fix#287 (#288)
    * Update get_retweeted_by.R
    
    Modified the script to avoid 400 error by incorporating pagination following a recent update to the API. Formatted using same conventions as `get_liking_users`.
    
    * Fix identation
    
    * Make `get_retweeted_by` respect verbose
    
    * Retain the previous zero row behavior for `get_retweeted_by`
    
    * Update previous test cases
    
    * Fix the integration bug of `get_retweeted_by`
    
    * Remove redundant documentation in `get_retweeted_by`
    
    * Make Thomas R Davidson a contributor
    
    * Update Davidson's ORCID
    
    Co-authored-by: chainsawriot <chainsawtiney@gmail.com>

--- tests/testthat/test-get_retweeted_by.R
+++ tests/testthat/test-get_retweeted_by.R
@@ -1,7 +1,7 @@
-# require(httptest)
-# start_capturing(simplify = FALSE)
-# res <- get_retweeted_by("1423714771384340481")
-# stop_capturing()
+## require(httptest)
+## start_capturing(simplify = FALSE)
+## res <- get_retweeted_by("1423714771384340481")
+## stop_capturing()
 
 with_mock_api({
   test_that("expected behavior, #153", {
@@ -37,5 +37,36 @@ with_mock_api({
     skip_if(!dir.exists("api.twitter.com"))
     expect_silent(get_retweeted_by(c("1399797349296197635", "1423714771384340481"), verbose = FALSE))
     expect_output(get_retweeted_by(c("1399797349296197635", "1423714771384340481"), verbose = TRUE))
+  })
+})
+
+## issue #287
+
+## require(httptest)
+## start_capturing(simplify = FALSE)
+## res <- get_retweeted_by("1476155918597373952")
+## stop_capturing()
+
+with_mock_api({
+  test_that("issue #287", {
+    skip_if(!dir.exists("api.twitter.com"))
+    expect_silent(get_retweeted_by(c("1476155918597373952"), verbose = FALSE))
+    expect_output(get_retweeted_by(c("1476155918597373952"), verbose = TRUE))
+  })
+})
+
+## require(httptest)
+## start_capturing(simplify = FALSE)
+## res <- get_retweeted_by("1483379681206415361")
+## stop_capturing()
+
+with_mock_api({
+  test_that("issue #287, integration", {
+    skip_if(!dir.exists("api.twitter.com"))
+    ids <- c("1399797349296197635", "1476155918597373952", "1483379681206415361")
+    expect_output(res <- get_retweeted_by(ids, verbose = TRUE))
+    expect_false("1399797349296197635" %in% unique(res$from_id))
+    expect_true("1476155918597373952" %in% unique(res$from_id))
+    expect_true("1483379681206415361" %in% unique(res$from_id))
   })
 })
